11130d48b5da55402d91ec48a1885acd67136305
Razamindset <razamindset.official@gmail.com>
2025-10-28 11:59:12 +0500

    Chore: updated description

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Downloadyt",
-  description: "A youtube Video Downloader",
+  description: "A youtube Video Downloader, by Ali Raza",
 };
 
 export default function RootLayout({ children }) {
